714640c0098fc00a509f48140cda66e597e975fb
chelokot <andrey.vlasenko.work@gmail.com>
2025-09-06 20:54:52 +0300

    fix(edge build): remove @grammyjs/i18n runtime import in edge; use MicroI18n to avoid Node built-ins in esbuild bundle

--- src/config/locale-engine.edge.ts
+++ src/config/locale-engine.edge.ts
@@ -1,12 +1,8 @@
-import { I18n } from '@grammyjs/i18n';
+import { MicroI18n } from './i18n.micro.js';
 import en from '../locales/en.js';
 
 export function initLocaleEngineEdge(defaultLanguage = 'en') {
-  const i18n = new I18n({
-    defaultLanguage,
-    defaultLanguageOnMissing: true,
-    useSession: false,
-  });
+  const i18n = new MicroI18n({ defaultLanguage });
   i18n.loadLocale('en', en as any);
   return i18n;
 }
